6baf328013087e1260ec0bd35884ddd1385f5fe2
mDrone <ogebhard@stud.hs-offenburg.de>
2013-10-23 20:31:02 +0200

    terminal

---
+++ Terminal.java
@@ -0,0 +1,21 @@
+/**
+ * File Name     : Terminal.java
+ * Purpose       :
+ * Creation Date : 23-10-2013
+ * Last Modified : Wed 23 Oct 2013 08:30:34 PM CEST
+ * Created By    :
+ *
+ */
+
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+
+
+public class Terminal {
+	public static void main( String[] args ) {
+		System.out.println("Hello World!");	
+	}
+}
