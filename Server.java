6728e411024ee1b08fb023882a3e380936f1d13d
mDrone <ogebhard@stud.hs-offenburg.de>
2013-10-26 18:50:54 +0200

    t

--- Server.java
+++ Server.java
@@ -2,20 +2,36 @@
  * File Name     : Server.java
  * Purpose       :
  * Creation Date : 26-10-2013
- * Last Modified : Sat 26 Oct 2013 01:44:58 PM CEST
+ * Last Modified : Sat 26 Oct 2013 05:50:41 PM CEST
  * Created By    :
  *
  */
 
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
+import java.io.*;
+import java.net.*;
 
 
 public class Server {
     public static void main( String[] args ) {
-        System.out.println("Hello World!"); 
+        int portNumber = 1024;
+
+        try (
+            ServerSocket serverSocket = new ServerSocket(portNumber);
+
+            Socket clientSocket = serverSocket.accept();
+
+            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+            ) {
+                String inputLine;
+                while ((inputLine = in.readLine()) != null) {
+                    out.println(inputLine);
+                }
+            } catch (IOException e) {
+                System.out.println("Exception caught when trying to listen on port " + portNumber + " or listening for a connection");
+                System.out.println(e.getMessage());
+            }
     }
 }
--- Terminal.java
+++ Terminal.java
@@ -2,20 +2,39 @@
  * File Name     : Terminal.java
  * Purpose       :
  * Creation Date : 23-10-2013
- * Last Modified : Sat 26 Oct 2013 01:45:03 PM CEST
+ * Last Modified : Sat 26 Oct 2013 05:39:15 PM CEST
  * Created By    :
  *
  */
 
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.ArrayList;
+import java.io.*;
+import java.net.*;
 
 
 public class Terminal {
-    public static void main( String[] args ) {
-        System.out.println("Hello World!"); 
+    public static void main(String[] args) {
+        String hostName = "localhost";
+        int portNumber = 1024;
+
+        try (
+            Socket socket = new Socket(hostName, portNumber);
+            PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+            BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+            BufferedReader stdIn = new BufferedReader(new InputStreamReader(System.in));
+            ) {
+            String userInput;
+            while ((userInput = stdIn.readLine()) != null) {
+                out.println(userInput);
+                System.out.println("echo: " + in.readLine());
+            }
+        } catch (UnknownHostException e) {
+            System.err.println("Don't know about host " + hostName);
+            System.exit(1);
+        } catch (IOException e) {
+            System.err.println("Couldn't get I/O for the connection to " + hostName);
+            System.exit(1);
+        }
     }
 }
